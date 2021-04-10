03200127f0719408207373ff1d2342695f63577f
Edmar Diaz <diaz.edmar@outlook.com>
2021-04-11 07:42:24 +0800

    feat(exception): write error logs on a file using winston

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,33 @@ import { PermissionModule } from './modules/permission/permission.module';
 import * as ormconfig from './ormconfig';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './shared/filters';
+import * as winston from 'winston';
+import { WinstonModule } from 'nest-winston';
+import 'dotenv/config'
+import { NODE_ENV } from './shared/enums';
+
+let winstoTransports = []
+
+if(process.env.ENV === NODE_ENV.PRODUCTION) {
+  winstoTransports = [
+    new winston.transports.File({ filename: `logs/${new Date().toLocaleDateString().replace(/\//g, '-')}/error.log`, level: 'error' })
+  ]
+}else {
+  winstoTransports = [
+    new winston.transports.Console()
+  ]
+}
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig),
+    WinstonModule.forRoot({
+      transports: winstoTransports
+    }),
     AuthModule,
     UserModule,
     RoleModule,
-    PermissionModule
+    PermissionModule,
   ],
   controllers: [AppController],
   providers: [
--- src/shared/filters/http-exception.filter.ts
+++ src/shared/filters/http-exception.filter.ts
@@ -1,10 +1,13 @@
-import { ExceptionFilter, Catch, HttpException, ArgumentsHost, HttpStatus, Logger } from '@nestjs/common';
+import { ExceptionFilter, Catch, HttpException, ArgumentsHost, HttpStatus, Logger, Inject } from '@nestjs/common';
+import { Logger as WinstonLogger } from 'winston';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   
   private readonly logger = new Logger(HttpExceptionFilter.name);
   
+  constructor(@Inject('winston') private readonly winstonLogger: Logger) {}
+
   catch(error: Error, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const request = ctx.getRequest();
@@ -12,13 +15,15 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const status = (error instanceof HttpException) ? error.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
     if (status === HttpStatus.INTERNAL_SERVER_ERROR) {
-      this.logger.error(error.stack)
-      return response.status(500).json({
+      const err = {
         statusCode: 500,
         timestamp: new Date().toISOString(),
         path: request.url,
         message: String(error.stack)
-      })
+      }
+      this.logger.error(error.stack)
+      this.winstonLogger.error(err)
+      return response.status(500).json(err)
     }
 
     return response.status(status).json({
